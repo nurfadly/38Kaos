3e68c2d4c68ef932355413dcca74098fb48351fa
arno <suwarno.habe@kalla.co.id>
2026-07-04 22:07:40 +0800

    Fix CORS: pakai origin string '*' statis, tidak bergantung header Origin request (fix Codespaces tunnel proxy)

--- cms/tigalapankaos-cms/config/middlewares.js
+++ cms/tigalapankaos-cms/config/middlewares.js
@@ -18,8 +18,16 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      // Ganti '*' dengan domain website production kamu, misal: ['https://tigalapankaos.id']
-      origin: ['*'],
+      // Pakai string '*' (bukan array) supaya header Access-Control-Allow-Origin
+      // SELALU dikirim apa adanya, tidak bergantung pada header Origin dari request.
+      // Ini penting karena proxy GitHub Codespaces/dev tunnel kadang menghapus
+      // header Origin sebelum sampai ke Strapi, sehingga pengecekan berbasis
+      // array (mencocokkan ke Origin request) gagal dan header CORS tidak terkirim.
+      // Ganti dengan domain website production kamu kalau sudah live permanen,
+      // misal: 'https://tigalapankaos.id'
+      origin: '*',
+      headers: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
     },
   },
   'strapi::poweredBy',
